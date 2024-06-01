1fbc1e5232368dd74902a8ec6132ba5f8a58452d
Muntasir Chowdhury <muntasirornob@gmail.com>
2024-05-31 20:22:54 -0600

    adding files

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+
+    public function __construct()
+    {
+        //
+    }
+    public function index()
+    {
+        
+    }
+
+    public function show()
+    {
+        
+    }
+
+    public function store()
+    {
+    
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function destroy()
+    {
+    
+    }
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Book;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    use ApiResponser;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function show()
+    {
+        
+    }
+
+    public function store()
+    {
+    
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function destroy()
+    {
+    
+    }
+
+   }
